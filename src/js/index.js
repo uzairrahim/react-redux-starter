87b8e66b4d3c75ce34c6912f7021a3ac2b7d4492
Uzair Rahim <uzair.rahim@hotschedules.com>
2017-01-03 09:36:14 -0600

    initial commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,7 @@
 import '../scss/index.scss';
 import './axios.config';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import Store from 'store';
